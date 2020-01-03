15997fc18822c6d9941bf330ecd80b0d88e9afa8
adlemon <alexander.lemon.ii@gmail.com>
2020-01-02 21:30:06 -0800

    Exercises for Accelerated C++ Chapter 0.

---
+++ programming/cpp/exercises/accelerated_cpp/00_getting_started/print_tabs.cpp
@@ -0,0 +1,14 @@
+// print_tabs.cpp
+// Print tabs to the console.
+
+#include <iostream>
+
+int main()
+{
+  std::cout << "\t|" << std::endl;
+  std::cout << "|\t|" << std::endl;
+  std::cout << "|\t|\t|" << std::endl;
+  std::cout << "|\t|\t|\t|" << std::endl;
+  std::cout << "|\t|\t|\t|\t|" << std::endl;
+  return 0;
+}
